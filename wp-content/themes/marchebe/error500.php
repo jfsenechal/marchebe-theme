fd81787b0147b2e2a3fad20167e288133b23cec0
jfsenechal <jfsenechal@gmail.com>
2025-10-08 15:14:48 +0200

    up

--- wp-content/themes/marchebe/error500.php
+++ wp-content/themes/marchebe/error500.php
@@ -1,2 +1,20 @@
 <?php
+
 namespace AcMarche\Theme;
+
+use AcMarche\Theme\Lib\Twig;
+
+get_header();
+$twig = Twig::loadTwig();
+try {
+    echo $twig->render('@AcMarche/homepage.html.twig', [
+        'statusCode' => $statusCode,
+        'statusText' => $statusText,
+    ]);
+} catch (LoaderError|RuntimeError|SyntaxError $e) {
+    echo $e->getMessage();
+}
+echo $statusCode;
+echo $statusText;
+
+get_footer();
