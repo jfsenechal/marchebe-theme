ecf33162654d7f62bd88d43a950cbe40701960cc
Senechal jf <jfsenechal@gmail.com>
2025-11-20 13:23:00 +0100

    rename

--- wp-content/themes/marchebe/Inc/Ajax.php
+++ wp-content/themes/marchebe/Inc/Ajax.php
@@ -8,27 +8,27 @@ class Ajax
 {
     public function __construct()
     {
-        add_action('wp_enqueue_scripts', [$this, 'my_enqueue_scripts']);
+        add_action('wp_enqueue_scripts', [$this, 'setCategoryScript']);
         // Handle the AJAX request
-        add_action('wp_ajax_my_ajax_action', [$this, 'my_ajax_handler']); // For logged-in users
-        add_action('wp_ajax_nopriv_my_ajax_action', [$this, 'my_ajax_handler']); // For non-logged users
+        add_action('wp_ajax_set_category_action', [$this, 'setCategoryHandler']); // For logged-in users
+        add_action('wp_ajax_nopriv_set_category_action', [$this, 'setCategoryHandler']); // For non-logged users
     }
 
     // Localize script to pass Ajax URL and nonce
-    function my_enqueue_scripts()
+    public function setCategoryScript(): void
     {
         // admin_url('admin-ajax.php'), //bug route frankenphp
         $url = 'https://'.$_ENV['WP_URL_HOME'].'/wp-admin/admin-ajax.php';
 
         wp_localize_script('marchebe-alpine', 'wpData', array(
             'ajaxUrl' => $url,
-            'nonce' => wp_create_nonce('my_ajax_nonce'),
+            'nonce' => wp_create_nonce('set_category_nonce'),
         ));
     }
 
-    function my_ajax_handler()
+    public function setCategoryHandler(): void
     {
-        check_ajax_referer('my_ajax_nonce', 'nonce');
+        check_ajax_referer('set_category_nonce', 'nonce');
 
         $categoryId = isset($_POST['categorySelected']) ? intval($_POST['categorySelected']) : 0;
         $currentSite = isset($_POST['currentSite']) ? intval($_POST['currentSite']) : Theme::CITOYEN;
