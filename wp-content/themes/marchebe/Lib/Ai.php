c19cbdc855082f6a4f86e21e00416479b784abfc
Senechal jf <jfsenechal@gmail.com>
2025-11-24 11:46:37 +0100

    listen

---
+++ wp-content/themes/marchebe/Lib/Ai.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AcMarche\Theme\Lib;
+
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class Ai
+{
+    private HttpClientInterface $client;
+
+    public function __construct()
+    {
+
+    }
+
+    private function connect():void
+    {
+        $this->client = HttpClient::createForBaseUri($_ENV['OPENAI_API_URL']);
+    }
+
+    /**
+     * curl https://api.openai.com/v1/audio/speech \
+     * -H "Authorization: Bearer $OPENAI_API_KEY" \
+     * -H "Content-Type: application/json" \
+     * -d '{
+     * "model": "gpt-4o-mini-tts",
+     * "input": "Today is a wonderful day to build something people love!",
+     * "voice": "coral",
+     * "instructions": "Speak in a cheerful and positive tone."
+     * }' \
+     * --output speech.mp3
+     * @return void
+     */
+    public function textToSpeech()
+    {
+
+    }
+
+}
